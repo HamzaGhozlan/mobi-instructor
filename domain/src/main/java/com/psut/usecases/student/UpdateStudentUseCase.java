47b15c0e584c45182a4822171e4a42eada370075
Hamza Ghozlan <ham.ghozlan@gmail.com>
2023-03-04 14:42:58 +0300

    fix: validate username in updating use case

--- domain/src/main/java/com/psut/usecases/student/UpdateStudentUseCase.java
+++ domain/src/main/java/com/psut/usecases/student/UpdateStudentUseCase.java
@@ -1,10 +1,13 @@
 package com.psut.usecases.student;
 
+import com.psut.exceptions.BusinessValidationException;
 import com.psut.models.student.Student;
 import com.psut.models.student.UpdateStudentRequest;
 import com.psut.repositories.StudentRepository;
 import com.psut.validators.StudentValidator;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
 
 @RequiredArgsConstructor
 public class UpdateStudentUseCase {
@@ -13,7 +16,14 @@ public class UpdateStudentUseCase {
 
     public Student execute(Student student, UpdateStudentRequest updateRequest) {
         updateRequest.applyUpdatesOn(student);
-        studentValidator.validate(student);
+        validate(student);
         return studentRepository.save(student);
+    }
+
+    private void validate(Student student) {
+        Set<String> violations = studentValidator.validate(student);
+        if (!violations.isEmpty()) {
+            throw new BusinessValidationException(violations);
+        }
     }
 }
