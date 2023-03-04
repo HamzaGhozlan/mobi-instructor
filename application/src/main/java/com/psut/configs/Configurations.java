9c7137531bbe5db6922e014f0563a09e5302ef51
Hamza Ghozlan <ham.ghozlan@gmail.com>
2023-03-04 15:22:23 +0300

    fix: nothing

--- application/src/main/java/com/psut/configs/Configurations.java
+++ application/src/main/java/com/psut/configs/Configurations.java
@@ -11,14 +11,13 @@ import com.psut.validators.UsernameValidator;
 import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-@EnableJpaRepositories("com.psut.repositories")
+//@EnableJpaRepositories("com.psut.repositories")
 public class Configurations implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
