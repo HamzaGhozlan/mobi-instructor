cfe3a88bf3efc37b7998a4bd9725e475ab4470a0
Hamza Ghozlan <ham.ghozlan@gmail.com>
2023-05-23 12:53:57 +0300

    NA

---
+++ target/generated-sources/annotations/com/psut/repository/mapper/MaterialMapperImpl.java
@@ -0,0 +1,59 @@
+package com.psut.repository.mapper;
+
+import com.psut.model.Material;
+import com.psut.repository.entity.MaterialEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-23T12:51:34+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
+)
+@Component
+public class MaterialMapperImpl implements MaterialMapper {
+
+    @Override
+    public MaterialEntity toEntity(Material material) {
+        if ( material == null ) {
+            return null;
+        }
+
+        MaterialEntity materialEntity = new MaterialEntity();
+
+        materialEntity.setId( material.getId() );
+        materialEntity.setCategory( material.getCategory() );
+        materialEntity.setTitle( material.getTitle() );
+        List<String> list = material.getStages();
+        if ( list != null ) {
+            materialEntity.setStages( new ArrayList<String>( list ) );
+        }
+        materialEntity.setPricePerHour( material.getPricePerHour() );
+        materialEntity.setPricePerCourse( material.getPricePerCourse() );
+
+        return materialEntity;
+    }
+
+    @Override
+    public Material toDomain(MaterialEntity materialEntity) {
+        if ( materialEntity == null ) {
+            return null;
+        }
+
+        Material material = new Material();
+
+        material.setId( materialEntity.getId() );
+        material.setCategory( materialEntity.getCategory() );
+        material.setTitle( materialEntity.getTitle() );
+        List<String> list = materialEntity.getStages();
+        if ( list != null ) {
+            material.setStages( new ArrayList<String>( list ) );
+        }
+        material.setPricePerHour( materialEntity.getPricePerHour() );
+        material.setPricePerCourse( materialEntity.getPricePerCourse() );
+
+        return material;
+    }
+}
---
+++ target/generated-sources/annotations/com/psut/repository/mapper/StudentMapperImpl.java
@@ -0,0 +1,64 @@
+package com.psut.repository.mapper;
+
+import com.psut.model.student.Student;
+import com.psut.repository.entity.StudentEntity;
+import java.util.Arrays;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-23T12:51:34+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
+)
+@Component
+public class StudentMapperImpl implements StudentMapper {
+
+    @Override
+    public StudentEntity toEntity(Student student) {
+        if ( student == null ) {
+            return null;
+        }
+
+        StudentEntity studentEntity = new StudentEntity();
+
+        studentEntity.setId( student.getId() );
+        studentEntity.setFirstName( student.getFirstName() );
+        studentEntity.setLastName( student.getLastName() );
+        studentEntity.setGender( student.getGender() );
+        studentEntity.setUsername( student.getUsername() );
+        studentEntity.setPassword( student.getPassword() );
+        studentEntity.setStatus( student.getStatus() );
+        studentEntity.setEmail( student.getEmail() );
+        byte[] image = student.getImage();
+        if ( image != null ) {
+            studentEntity.setImage( Arrays.copyOf( image, image.length ) );
+        }
+
+        return studentEntity;
+    }
+
+    @Override
+    public Student toDomain(StudentEntity studentEntity) {
+        if ( studentEntity == null ) {
+            return null;
+        }
+
+        Student student = new Student();
+
+        student.setId( studentEntity.getId() );
+        student.setFirstName( studentEntity.getFirstName() );
+        student.setLastName( studentEntity.getLastName() );
+        student.setGender( studentEntity.getGender() );
+        student.setUsername( studentEntity.getUsername() );
+        student.setPassword( studentEntity.getPassword() );
+        student.setStatus( studentEntity.getStatus() );
+        student.setEmail( studentEntity.getEmail() );
+        byte[] image = studentEntity.getImage();
+        if ( image != null ) {
+            student.setImage( Arrays.copyOf( image, image.length ) );
+        }
+
+        return student;
+    }
+}
